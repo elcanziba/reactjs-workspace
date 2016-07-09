b512344fc9aef0f941c0b4c2906475776e9fbd1b
Elvis Canziba <elvis554@gmail.com>
2016-07-10 02:44:39 +0400

    Added Video functionality and Select options.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,12 @@ import VideoListItem from './video_list_item';
 // Define Function
 const VideoList = (props) => {
 	const videoItems = props.videos.map((video) => {
-		return <VideoListItem key={video.etag} video={video} />
+		return (
+			<VideoListItem 
+			onVideoSelect = {props.onVideoSelect}
+			key={video.etag} 
+			video={video} />
+		);
 	});
 
 	return (
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 // Youtube API KEY
 const API_KEY = 'AIzaSyCXZvag6tNpUohUhRP2Z4kaCa170GbliLQ';
@@ -13,10 +14,16 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [] ,
+			selectedVideo: null
+		};
 
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 
@@ -24,7 +31,10 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
 			</div>
 		);
 	}
