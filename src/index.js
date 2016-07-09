5f6e0a0c8cc3c268fe4116a5f3a5ad2605d2fb10
Elvis Canziba <elvis554@gmail.com>
2016-07-10 01:24:28 +0400

    Update of API KEY + Refactoring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 
 // Youtube API KEY
 const API_KEY = 'AIzaSyCXZvag6tNpUohUhRP2Z4kaCa170GbliLQ';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+});
+
 // Create a new Component.
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 // Render on Dom
