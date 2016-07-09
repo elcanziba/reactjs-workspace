6a1ed60078f345a0a1cb3bc8496ea3b188411963
Elvis Canziba <elvis554@gmail.com>
2016-07-10 02:05:30 +0400

    Update of Video Images and Titles

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 // Youtube API KEY
 const API_KEY = 'AIzaSyCXZvag6tNpUohUhRP2Z4kaCa170GbliLQ';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data);
-});
-
 // Create a new Component.
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 // Render on Dom
